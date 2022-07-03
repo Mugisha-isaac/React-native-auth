3882bf60384cd96d8af797c86acd09cb168d6e2b
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-07-03 19:12:55 +0200

    [feat] splashing screen

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,32 +1,42 @@
-import React,{useContext} from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack' ;
-import HomeScreen from '../screens/HomeScreen';
-import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
+import React, { useContext } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import SplashScreen from "../screens/SplashScreen";
 import { AuthContext } from "../context/AuthContext";
-
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
-
-
-  const {userInfo} = useContext(AuthContext);
-  console.log(userInfo) 
+  const { userInfo, splashLoading } = useContext(AuthContext);
+  console.log(userInfo);
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {userInfo.token ? ( <Stack.Screen name='Home' component={HomeScreen} />):(
+        {splashLoading ? (
+          <Stack.Screen
+            name="Splash"
+            component={SplashScreen}
+            options={{ headerShown: false }}
+          />
+        ) : userInfo.token ? (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        ) : (
           <>
-        <Stack.Screen name='Login' component={LoginScreen} options={{headerShown:false}} />
-        <Stack.Screen name='Register' component={RegisterScreen} />
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Register" component={RegisterScreen} />
           </>
         )}
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-import React, { createContext } from "react";
+import React, { createContext, useEffect } from "react";
 import { BASE_URL } from "../config";
 
 export const AuthContext = createContext();
@@ -8,6 +8,7 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [isLoading, setIsLoading] = React.useState(false);
   const [userInfo, setUserInfo] = React.useState({});
+  const [splashLoading, setSplashLoading] = React.useState(false);
 
   const register = (name, email, password) => {
     setIsLoading(true);
@@ -29,26 +30,56 @@ export const AuthProvider = ({ children }) => {
       });
   };
 
-
-  const login = (email,password)=>{
+  const login = (email, password) => {
     setIsLoading(true);
 
-    axios.post(`${BASE_URL}/login`,{email,password}).then(res=>{
-      let userInfo = res.data;
-      console.log(userInfo)
-      setUserInfo(userInfo);
-      AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
-      setIsLoading(false);
-    }).catch(err=>{
-      console.log(`Login error ${err}`);
-      setIsLoading(false);
-    });
-  }
+    axios
+      .post(`${BASE_URL}/login`, { email, password })
+      .then((res) => {
+        let userInfo = res.data;
+        console.log(userInfo);
+        setUserInfo(userInfo);
+        AsyncStorage.setItem("userInfo", JSON.stringify(userInfo));
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(`Login error ${err}`);
+        setIsLoading(false);
+      });
+  };
 
+  // to implement it's backend api
+  // const logout = ()=>{
+  //     setIsLoading(true);
 
-  
+  // }
+
+  const isLoggedIn = async () => {
+    try {
+      setSplashLoading(true);
+      let userInfo = await AsyncStorage.getItem("userInfo");
+      userInfo = JSON.parse(userInfo);
+
+      if (userInfo) {
+        setUserInfo(userInfo);
+      }
+
+      setSplashLoading(false);
+    } catch (e) {
+      setSplashLoading(false);
+      console.log(`is Logged In error ${e}`);
+    }
+  };
+
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{register,login,isLoading,userInfo}}>{children}</AuthContext.Provider>
+    <AuthContext.Provider
+      value={{ register, login,splashLoading, isLoading, userInfo }}
+    >
+      {children}
+    </AuthContext.Provider>
   );
 };
